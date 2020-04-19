02a372d171e1a46f9d4c09ce66e8bb4dcc6db8b6
ska <samory.ka@lesfurets.com>
2020-04-19 16:27:50 +0200

    typescript migration - Content

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import About from './About/About';
+import Projects from './Projects/Projects';
+
+import './Content.css'
+
+const Content: FC = () => (
+    <div className='content'>
+        <Switch>
+            <Route exact={true} path={`${process.env.PUBLIC_URL}/`} render={() => <Redirect to='/about' />} />
+            <Route exact={true} path={`${process.env.PUBLIC_URL}/about`} component={About} />
+            <Route exact={true} path={`${process.env.PUBLIC_URL}/projects`} component={Projects} />
+        </Switch>
+    </div>
+);
+
+export default Content;
